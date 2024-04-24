f75713a853aa483967f793065972f573a03f35a5
Hanson <Hansonlau7@gmail.com>
2024-04-24 00:30:07 -0700

    images for sushi stack, nav bar in every html page

--- Project2Arcade/app.js
+++ Project2Arcade/app.js
@@ -3,12 +3,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const navButton = document.getElementById("reveal-nav");
     const navBar = document.getElementById("nav-bar");
 
+    const listGameSelection = document.getElementsByClassName("game-selection-container");
+    const gameSelection = listGameSelection[0];
+
     navButton.addEventListener("click", () => {
 
         console.log("hello");
 
         navBar.classList.toggle("hide");
         navButton.classList.toggle("hide");
+
+        gameSelection.classList.toggle("adjust");
 
     });
 });
