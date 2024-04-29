e544bf3231f23ab65cdb63f6b837fbaf0f0e124c
Hanson <Hansonlau7@gmail.com>
2024-04-28 17:11:19 -0700

    sushi stack essentials done, styling changes needed

--- Project2Arcade/menu-match.js
+++ Project2Arcade/menu-match.js
@@ -43,35 +43,102 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }
 
-        let countImagesPlaced = [0, 0, 0, 0, 0, 0];
+        let countImagesPlaced = [];
 
-        // adding click stuff ///////////////
-
-        for (let i = 0; i < cards.length; i++) {
-            cards[i].addEventListener("click", () => {
-
-                // chose image randomly
-                let index = Math.floor(Math.random() * chosenImages.length);
-
-                while (countImagesPlaced[index] >= matchSize) {
-                    index = Math.floor(Math.random() * chosenImages.length);
-                }
-
-                console.log("index: " + index);
-                countImagesPlaced[index]++;
-
-                cards[i].classList.add("card2");
-
-                setTimeout( function() {
-                    cards[i].classList.remove("card2");
-                    cards[i].classList.add(chosenImages[index]);
-
-                } , 500)
-
-            });
+        for (let i = 0; i < matches; i++) {
+            countImagesPlaced.push(0);
         }
 
-        /////////////////////////////////////
+        const checking = document.getElementsByClassName("checking");
+
+        for (let i = 0; i < cards.length; i++) {
+
+            // chose image randomly
+            let index = Math.floor(Math.random() * chosenImages.length);
+
+            while (countImagesPlaced[index] >= matchSize) {
+                index = Math.floor(Math.random() * chosenImages.length);
+            }
+
+            countImagesPlaced[index]++;
+            cards[i].classList.add(chosenImages[index]);
+            cards[i].classList.add("hide-image");
+
+            cards[i].addEventListener("click", () => {
+
+                if (!(cards[i].classList.contains("checking") || 
+                      cards[i].classList.contains("matched")  ||
+                      checking.length >= matchSize)) {
+                    cards[i].classList.add("card2");
+                    cards[i].classList.add("checking");
+
+                    setTimeout( function() {
+                        cards[i].classList.remove("card2");
+                        cards[i].classList.remove("hide-image");
+                    } , 500)
+
+                    checkMatch(matchSize, chosenImages[index]);
+                }
+                    
+            });
+        }
+    }
+
+    function checkMatch(matchSize, image) {
+
+        const checking = document.getElementsByClassName("checking");
+        const checkingLength = checking.length;
+        console.log("checking length: " + checking.length);
+        let matching = true;
+
+        // make sure every card selected has that image
+        for (let i = 0; i < checkingLength; i++) {
+
+            if (!checking[i].classList.contains(image)) {
+                console.log("image did not match")
+                // card doesn't have the same image
+                matching = false;
+                break;
+            }
+        }
+
+        if (matching) {
+
+            // check if all have been matched
+            if (checkingLength == matchSize) {
+                console.log("one set matched");
+                // take "checking" class out, add "matched" class
+                for (let i = 0; i < checkingLength; i++) {
+                    checking[0].classList.add("matched");
+                    checking[0].classList.remove("checking");
+                    // console.log("length now: " + checking.length);
+                }
+            }
+
+        } else {
+
+            // flip all cards with "checking" class over
+            for (let i = 0; i < checkingLength; i++) {
+
+                console.log("removing card");
+                setTimeout( function() {
+                    checking[i].classList.add("card2");
+                } , 1700);
+
+                setTimeout( function() {
+                    checking[i].classList.remove("card2");
+                    checking[i].classList.add("hide-image");
+                } , 2100);
+
+            }
+
+            for (let i = 0; i < checkingLength; i++) {
+                // console.log("length now: " + checking.length);
+                setTimeout( function() {
+                checking[0].classList.remove("checking");
+                } , 2100);
+            }
+        }
     }
 
     function makeGrid(rows, cols) {
